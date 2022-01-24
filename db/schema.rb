f8c57a9f860d82e9fdc9b65cd46f1aa5433f9a94
lauretteroy <laurieroy.dev@gmail.com>
2022-01-24 14:41:07 -0800

    Scaffold place

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_214633) do
+ActiveRecord::Schema.define(version: 2022_01_24_223830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 2022_01_24_214633) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "zip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "trips", force: :cascade do |t|

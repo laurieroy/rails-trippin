8ff258389d12048b19194c614920cc73aa3a6cef
lauretteroy <laurieroy.dev@gmail.com>
2022-01-24 18:54:41 -0800

    Add turbo to update

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,8 @@ class Place < ApplicationRecord
 	has_many :destinations
 	has_many :trips, through: :destinations
 	has_one_attached :image
+
+	after_update_commit do
+		broadcast_replace_to(:places)
+	end
 end

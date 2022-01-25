5863c91894dcbb0d87a8aee5d7039304a5afcc6e
lauretteroy <laurieroy.dev@gmail.com>
2022-01-24 20:03:57 -0800

    Add modal to new place

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,10 +1,12 @@
-import { controller } from "stimulus"
+import { controller } from "stimulus";
 
 export default class extends Controller {
-	static targets = ["frame", "container"]
-	close(e) {
-		e.preventDefault()
-		this.frameTarget.src = ""
-		this.containerTarget.remove()
-	}
+  static targets = ["frame", "container"];
+
+  close(e) {
+    e.preventDefault();
+
+    this.frameTarget.src = "";
+    this.containerTarget.remove();
+  }
 }
--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,6 @@ class Place < ApplicationRecord
 	has_many :trips, through: :destinations
 	has_one_attached :image
 
-	after_update_commit do
-		broadcast_replace_to(:places)
-	end
+	after_create_commit { broadcast_append_to(:places) }
+	after_update_commit { broadcast_replace_to(:places) }
 end
